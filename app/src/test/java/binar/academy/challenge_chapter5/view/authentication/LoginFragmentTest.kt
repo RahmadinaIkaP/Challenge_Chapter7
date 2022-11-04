c3aada294c26f7508aeafaf85b9b7579604e4a98
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2022-11-04 15:29:54 +0700

    add flavoring ( semoga ga error, pake laptop kantor ini :" )

---
+++ app/src/test/java/binar/academy/challenge_chapter5/view/authentication/LoginFragmentTest.kt
@@ -0,0 +1,11 @@
+package binar.academy.challenge_chapter5.view.authentication
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class LoginFragmentTest{
+
+}
